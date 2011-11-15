92204233e995f1c211e6f73871dfae8c5d5fa47c
Guilherme Ferreira <guilffer@gmail.com>
2011-11-15 12:12:30 -0800

    melhorando os feedbacks

--- client.c
+++ client.c
@@ -7,12 +7,12 @@ int main(int argc, char ** argv) {
   char *source = NULL;
 
   if (argc != 3) {
-    printf("%s server-ip message", argv[0]);
+    printf("%s server-ip message\n", argv[0]);
     exit(1);
   }
 
   do_client(argv[1], argv[2]);
   do_server(&source, &recv_msg);
-  printf("\nreceived message = %s\n",recv_msg);
+  printf("\nReceived Message = '%s'\n",recv_msg);
   return 0;
 }
--- server.c
+++ server.c
@@ -6,7 +6,7 @@ int main(void) {
   char *recv_msg = NULL;
   char *source = NULL;
   do_server(&source, &recv_msg);
-  printf("\nreceived message = %s\n",recv_msg);
+  printf("\nReceived Message = '%s'\n",recv_msg);
   do_client(source, recv_msg);
   return 0;
 }
